b9305a3406c11852aa6dfb8168e2845f86539e14
jonnyhuck <jonnyhuck@gmail.com>
2024-08-26 07:03:19 +0100

    towards parallel

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use std::f32;
 use numpy::PyArray2;
 use pyo3::prelude::*;
-// use rayon::prelude::*;
+use rayon::prelude::*;
 use pyo3::types::PyTuple;
 use numpy::ndarray::{Array2, Dim}; // do not use ndarray directly!!
+use std::sync::atomic::{AtomicBool, Ordering};
 
 /**
  * Convert image space coords (r, c) to coordinate space (x, y) for a given dataset
@@ -37,84 +38,67 @@ fn adjust_height(height: f32, distance_squared: f32, earth_diameter: f32, refrac
  */
 #[inline]
 fn line_of_sight(r0: i32, c0: i32, height0: f32, r1: i32, c1: i32, height1: f32, radius_px: f32,
-    dem_data: &Array2<f32>, height: usize, width: usize, output: &mut Array2<i32>) {
-
-    // initialise max_dydx to -inf
+    dem_data: &Array2<f32>, height: usize, width: usize, output: &Array2<AtomicBool>) {
     let mut max_dydx = f32::NEG_INFINITY;
 
-    // loop through each cell in a line
     for (r, c) in line_bresenham(r0, c0, r1, c1).skip(1) {
+        let dx_squared = ((c0 - c).pow(2) + (r0 - r).pow(2)) as f32;
+        let dx = dx_squared.sqrt();
 
-        // calculate distance
-         let dx_squared =((c0-c).pow(2) + (r0-r).pow(2)) as f32;
-         let dx = dx_squared.sqrt();
-
-        // stop if we run past the radius or off the end of the dataset
         if dx > radius_px || r < 0 || r >= height as i32 || c < 0 || c >= width as i32 {
             break;
         }
 
-        // calculate the dy/dx ratios with and without the height added
         let base_dydx = (adjust_height(dem_data[(r as usize, c as usize)], dx_squared, 12740000.0, 0.13) - height0) / dx;
         let tip_dydx = (adjust_height(dem_data[(r as usize, c as usize)] + height1, dx_squared, 12740000.0, 0.13) - height0) / dx;
 
-        // if tip dy/dx ratio is biggest so far, set cell to 1
         if tip_dydx > max_dydx {
-            output[(r as usize, c as usize)] = 1;
+            output[(r as usize, c as usize)].store(true, Ordering::Relaxed);
         }
 
-        // if base dy/dx is biggest so far, update it
         max_dydx = max_dydx.max(base_dydx);
     }
 }
+
 
 /**
  * calculate viewshed 
  */
 #[pyfunction]
-fn viewshed(py: Python, x0: f32, y0: f32, radius_m: f32, observer_height: f32, target_height: f32,
-    dem_data: &PyArray2<f32>, gdal_transform: &PyTuple) -> PyResult<Py<PyArray2<i32>>> {
-        
-    // get dimensions of the dataset
+fn viewshed(
+    py: Python, x0: f32, y0: f32, radius_m: f32, observer_height: f32, target_height: f32,
+    dem_data: &PyArray2<f32>, gdal_transform: &PyTuple
+) -> PyResult<Py<PyArray2<i32>>> {
     let [height, width] = dem_data.shape() else { 
         panic!("DEM data not 2D")
     };
-    
-    // extract transform data
-    let gt: Vec<f32> = gdal_transform.extract()?;
 
-    // convert coords to image space and verify that we are inside the dataset
+    let gt: Vec<f32> = gdal_transform.extract()?;
     let [r0, c0] = to_image(&gt, x0, y0);
     if r0 < 0 || r0 >= *height as i32 || c0 < 0 || c0 >= *width as i32 {
         panic!("Coordinates are out of bounds.");
     }
 
-    // convert radius to image space
-    let radius_px = (radius_m / gt[1]) as i32;
-
-    // extract dem_data to ndarray
+    let res = gt[1];
+    let radius_px = (radius_m / res) as i32;
     let dem_data = dem_data.to_owned_array();
-
-    // get height of origin cell
     let height0 = dem_data[(r0 as usize, c0 as usize)] + observer_height;
 
-    // init output surface and set origin to 1
-    let mut output = Array2::<i32>::zeros(Dim((*height, *width)));
-    output[(r0 as usize, c0 as usize)] = 1;
+    // Use AtomicBool array for output
+    let output = Array2::from_shape_fn((height, width), |_| AtomicBool::new(false));
+    output[(r0 as usize, c0 as usize)].store(true, Ordering::Relaxed);
 
-    run a line of sight to each point in the perimeter
-    for (r, c) in circle_perimeter(r0, c0, radius_px * 3) { // TODO: is *3 necessary / best?
-        line_of_sight(r0, c0, height0, r, c, target_height, radius_px as f32, &dem_data, *height, *width, &mut output);
-    }
-    // circle_perimeter(r0, c0, radius_px * 3)
-    // .par_iter()
-    // .for_each(|&(r, c)| {
-    //     line_of_sight(r0, c0, height0, r, c, target_height, radius_px as f32, &dem_data, *height, *width, &mut output);
-    // });
+    circle_perimeter(r0, c0, radius_px * 3)
+        .par_iter()
+        .for_each(|&(r, c)| {
+            line_of_sight(r0, c0, height0, r, c, target_height, radius_px as f32, &dem_data, *height, *width, &output);
+        });
 
-    // Convert to PyArray2 and return
+    // Convert AtomicBool array back to i32
+    let output = output.map(|atomic| atomic.load(Ordering::Relaxed) as i32);
+
     let output_array = PyArray2::<i32>::from_owned_array(py, output);
-    return Ok(output_array.to_owned());
+    Ok(output_array.to_owned())
 }
 
 /**
